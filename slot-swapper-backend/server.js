c02ac92b0cfaecc4f9147c744bed916205fc3917
MBARC15 <maheshnjavalaga@gmail.com>
2025-11-06 18:49:34 +0530

    env updated

--- slot-swapper-backend/server.js
+++ slot-swapper-backend/server.js
@@ -1,36 +1,36 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import cors from 'cors';
 import UserRoutes from './routes/user.route.js';
 import EventRoutes from './routes/event.route.js';
 import SwapRoutes from './routes/swap.route.js';
-import cors from 'cors';
 
-const app = express()
-// const app = mongoose.connection;
+// Load environment variables from .env
+dotenv.config();
+
+const app = express();
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
+// MongoDB connection
 const connectDB = async () => {
-    try{
-        await mongoose.connect('mongodb://localhost:27017/slotswap',{
-
-        })
-        console.log("MongoDB connected Successfully!");
+    try {
+        await mongoose.connect(process.env.MONGO_URI, { });
+        console.log("MongoDB connected successfully!");
     } catch (error) {
-        console.error("MongoDB connection Failed:", error.message);
+        console.error("MongoDB connection failed:", error.message);
         process.exit(1);
     }
-}
+};
 
-connectDB()
+connectDB();
 
 UserRoutes(app);
 EventRoutes(app);
 SwapRoutes(app);
 
-const PORT = 5000;
-
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-})
+});
