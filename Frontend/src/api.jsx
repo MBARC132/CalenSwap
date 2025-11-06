721207b8970ed5687750a5bb6474d1c10dee8aaa
Mahesh Javalaga <156232064+MBARC132@users.noreply.github.com>
2025-11-06 19:02:15 +0530

    Update api.jsx

--- Frontend/src/api.jsx
+++ Frontend/src/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://calenswap.onrender.com" });
 
 // Include token in every request if available
 API.interceptors.request.use((req) => {
